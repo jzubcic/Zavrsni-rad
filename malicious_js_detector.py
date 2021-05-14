6b203b92d4b12b8cec18155a6794eff562fac114
Jakov Zubčić <jakov.zubcic@fer.hr>
2021-05-14 17:03:08 +0200

    Add option to give url stored in Websecradar DB as argument

--- malicious_js_detector.py
+++ malicious_js_detector.py
@@ -5,7 +5,9 @@ import requests
 import yara
 import jsbeautifier
 import urllib.request
+import pymongo
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 from entropy_calc import calculate_entropy
 from urllib.parse import urljoin
 from urllib.error import HTTPError
@@ -17,7 +19,6 @@ def clamscan_file(file):
     :param file: file to be scanned
     :return:
     """
-    """
     cd = clamd.ClamdUnixSocket()
     open('/tmp/EICAR', 'wb').write(clamd.EICAR)
     scan_result = cd.scan(file).__str__()
@@ -26,8 +27,6 @@ def clamscan_file(file):
         print(f'ClamAV\'s output: {scan_result}')
     else:
         print('ClamAV has not detected malicious code.')
-    """
-    pass
 
 
 def yara_scan(file: str):
@@ -36,6 +35,8 @@ def yara_scan(file: str):
         matches = rules.match(data=f.read())
         if matches:
             print("Malicious code has been detected by applying YARA rules.")
+        else:
+            print("No malicious code has been detected by applying YARA rules.")
 
 
 def load_from_file(file: str):
@@ -56,6 +57,8 @@ def perform_detection(file: str):
     entropy_after = calculate_entropy('temp.js')
     if abs(entropy_before - entropy_after) > 0.5:
         print("Warning! JS code was obfuscated, which can be a sign of malware.")
+    else:
+        print("Code was not obfuscated.")
     clamscan_file('temp.js')
     yara_scan('temp.js')
 
@@ -84,17 +87,42 @@ def load_from_webpage(url: str):
         perform_detection(js_file)
 
 
+
+
+def load_from_mongodb(url: str):
+    client = MongoClient("mongodb://rouser:MiLaBiLaFiLa123@127.0.0.1:27017/websecradar?authSource=websecradar")
+    db = client['websecradar']
+    url_collection = db['crawled_data_urls_v0']
+    document = url_collection.find_one({"url": url})
+    checks = document["checks"]
+    hash_set = set()
+    # scan each check from database, but only once per version (per same hash)
+    for check in checks:
+        hash_set.add(check['hash'])
+
+    pages_collection = db['crawled_data_pages_v0']
+    for hash in hash_set:
+        web_page = pages_collection.find_one({"hash": hash})
+        # print(web_page['page'])
+        with open('temp', 'w') as f:
+            f.write(web_page['page'])
+        perform_detection('temp')
+
+
 def main():
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-f', '--file', help='Provide path to HTML/JS file')
     group.add_argument('-w', '--web', action='store', type=str, help='Provide URL of website')
+    group.add_argument('-m', '--mongo', action='store', type=str, help='Provide URL stored in websecradar DB')
     args = parser.parse_args()
 
     if args.file is not None:
         load_from_file(args.file)
     elif args.web is not None:
         load_from_webpage(args.web)
+    elif args.mongo is not None:
+        load_from_mongodb(args.mongo)
 
 
 if __name__ == '__main__':
